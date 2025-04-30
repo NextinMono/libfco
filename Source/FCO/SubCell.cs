f85bec4d798c53b8093d1841e008122943819c73
Mahad <110350143+Hedgeturd@users.noreply.github.com>
2025-05-01 00:47:56 +0100

    Added SubCell Support

---
+++ Source/FCO/SubCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Amicitia.IO;
+using Amicitia.IO.Binary;
+
+namespace SUFcoTool
+{
+    public class SubCell : IBinarySerializable
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public int[] SubMessage { get; set; }
+
+        public SubCell()
+        {
+            SubMessage = [];
+        }
+        public void Read(BinaryObjectReader reader)
+        {
+            Start = reader.ReadInt32();
+            End = reader.ReadInt32();
+            
+            int messageLength = reader.ReadInt32();
+            int[] subcellMessageBytes = reader.ReadArray<int>(messageLength);
+            SubMessage = subcellMessageBytes;
+        }
+
+        public void Write(BinaryObjectWriter binaryWriter)
+        {
+            binaryWriter.Write(Start);
+            binaryWriter.Write(End);
+            
+            // Message Data
+            binaryWriter.Write(SubMessage.Length);
+            binaryWriter.WriteArray(SubMessage);
+        }
+    }
+}
